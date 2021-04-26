89f1947df1ce1b395d3a48a8be6416f8c9db49a0
patrykz1980 <83201554+patrykz1980@users.noreply.github.com>
2021-04-26 14:52:08 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD><TITLE>Magnifique Agency</TITLE>
+<META content="text/html; charset=iso-8859-2" http-equiv=Content-Type>
+<META http-equiv=pragma content=no-cache>
+<META content="Magnifique Agency, 4875 W. Armitage Ave., Chicago, IL, 60639, nanny service, children nanny service, eldely nanny service, Housekeeping and Cleaning services, full time nanny service, part time nanny service, live in nanny servcie, live out nanny service, licensed nanny service, bonded nanny service, insured nanny service, live-in nanny, reliable alternative to conventional daycare, Live-in nannies,  elderly daycare, special needs nanny service, childcare services" name=KEYWORDS>
+<META 
+content="Magnifique Agency 4875 W. Armitage Ave. Chicago IL 60639, is a nanny service company for children and the eldely. We also offer Housekeeping and Cleaning services which could be full time, part time, live in or live out. Our company is licensed, bonded and insured. Families are more complicated now than ever. Many women work while they are raising their children, and elect to hire help. The more help they have, the more bonds form between nannies and children, and the line between employer and family member blurs. That is why forethought and planning will help you hire a nanny who is right for you and your family. Magnifique Agency is that nanny service"
+name=DESCRIPTION>
+
+<META content=Magnifique Agency name=author>
+<META content="index, follow" name=ROBOTS>
+<link href="styl.css" rel="stylesheet" type="text/css">
+<META content="MSHTML 6.00.2900.2963" name=GENERATOR></HEAD>
+
+
+<style type="text/css">
+
+BODY {
+background-color: #FFFFFF;
+background-image: url(tlo.gif);
+background-repeat: repeat-x;
+margin: 0px;
+
+scrollbar-face-color: #f773ab;
+scrollbar-highlight-color: #f773ab;
+scrollbar-3dlight-color: #FFFFFF;
+scrollbar-darkshadow-color: #f773ab;
+scrollbar-shadow-color: #FFFFFF;
+scrollbar-arrow-color: #FFFFFF;
+scrollbar-track-color: #EEEFF0;
+
+}
+
+.index {color: #797375;
+	
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	margin-top: 0px;
+	margin-left: 0px;
+}
+
+.form {color: #797375;
+	
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 17px;
+	margin-top: 0px;
+	margin-left: 0px;
+}
+
+.menu {color: #797375;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	margin-top:0px;
+	margin-left: 0px;
+	margin-right: 0px;
+}
+
+
+.menux {color: #FFFFFF;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	margin-top:0px;
+	margin-left: 0px;
+	margin-right: 0px;
+}
+	
+.footer {color: #FFFFFF;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 11px;
+	letter-spacing: 0px;
+	
+	
+}
+
+.maps {color: #865d29;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	letter-spacing: 1px;
+	text-align: center;
+}
+
+
+.white {color: #FFFFFF;
+    font-family: Arial, Helvetica, sans-serif;
+	font-size: 16px;      
+	margin-left: 0px;
+	margin-right: 5px;
+        
+}
+
+.white2 {color: #FFFFFF;
+    font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;      
+	margin-left: 0px;
+	margin-right: 0px;
+        
+}
+
+
+.form {color: #494849;
+    font-family: Arial, Helvetica, sans-serif;
+	font-size: 11px;      
+	margin-left: 0px;
+	
+       
+}
+
+
+
+
+td.t1  { background-color: #f4eae4 }
+td.t2  { background-color: #f773ab }
+td.t3  { background-color: #cc0e58 }
+td.t4  { background-color: #f773ab }
+
+td.pasek1-pion  { background-color: #faa6c9 }
+td.pasek2-pion  { background-color: #f661a0 }
+
+	
+a.menu1:link {text-decoration: none; color:#797375; text-align: center; font-size: 13px;}
+a.menu1:visited {text-decoration: none; color:#797375; text-align: center; font-size: 13px;}
+a.menu1:hover {text-decoration: underline; color: #f773ab; text-align: center; font-size: 13px;}
+
+a.menu4:link {text-decoration: none; color:#FFFFFF; text-align: center; font-size: 12px;}
+a.menu4:visited {text-decoration: none; color:#FFFFFF; text-align: center; font-size: 12px;}
+a.menu4:hover {text-decoration: underline; color: #FFFFFF; text-align: center; font-size: 12px;}
+
+a.menu2:link {text-decoration: none; color:#FFFFFF; text-align: center; font-size: 12px;}
+a.menu2:visited {text-decoration: none; color:#FFFFFF; text-align: center; font-size: 12px;}
+a.menu2:hover {text-decoration: underline; color: #FFFFFF; text-align: center; font-size: 12px;}
+
+a.menu3:link {text-decoration: none; color:#B22222}
+a.menu3:visited {text-decoration: none; color:#B22222}
+a.menu3:hover {text-decoration: underline; color: #82a5bb}
+
+a.email:link {text-decoration: underline; color:#B22222}
+a.email:visited {text-decoration: none; color:#B22222}
+a.email:hover {text-decoration: none; color: #82a5bb}
+
+
+a.wiecej:link {text-decoration: none; color:#E67817; text-align: right;}
+a.wiecej:visited {text-decoration: none; color:#FFFFFF; text-align: right;}
+a.wiecej:hover {text-decoration: underline; color: #82a5bb; text-align: right;}
+
+
+input.pole_form {font-family: tahoma, helvetica, verdana, arial; font-size:11px; color: #FFFFFF; text-decoration: none; background-color: #6f6f6f}
+input.pole_form2 {font-family: tahoma, helvetica, verdana, arial; font-size:11px; color: #6f6f6f; text-decoration: none;  background-color:FFFFFF}
+
+input.pole {font-family: tahoma, arial, verdana, helvetica; font-size:11px; text-decoration: none; color: #6f6f6f; border: 1px #DBDBDB solid; background-color: #c7c7c7}
+textarea.tekst {font-family: tahoma, arial, verdana, helvetica; font-size:11px; text-decoration: none; color: #6f6f6f; border: 1px #DBDBDB solid; background-color: #c7c7c7}
+select.tekst {font-family: tahoma, arial, verdana, helvetica; font-size:11px; text-decoration: none; color: #6f6f6f; border: 1px #DBDBDB solid; background-color: #c7c7c7}
+textarea.tekst {font-family: tahoma, arial, verdana, helvetica; font-size:11px; text-decoration: none; color: #DBDBDB; border: 1px #6f6f6f solid; background-color:#FFFFFF}
+
+</style>
+
+
+
+</HEAD>
+
+
+
+<BODY bgcolor="pink" leftmargin="0" topmargin="0" marginheight=0>
+
+<TABLE align="center" cellSpacing=0 cellPadding=0 width="800" height="399" border=0>
+<tr>
+<td background= "images/agency_01.jpg" width="800" height="399">
+</td>
+</tr> 
+</table>
+
+
+
+<TABLE align="center" cellSpacing=0 cellPadding=0 width="800" height="57" border=0>
+<tr>
+<td background="images/agency_02.jpg" width="800" height="57">
+
+<center><font class="menu"><a class="menu1" href="index.php">Home</a> | <a class="menu1" href="service.php">Services</a> | <a class="menu1" href="plans.php">Plans</a> | <a class="menu1" href="area.php">Coverage Area</a> | <a class="menu1" href="contact.php">Contact Us</a></font></center>
+</td>
+</tr> 
+</table>
+
+
+
+<TABLE align="center" cellSpacing=0 cellPadding=0 width="800" height="400" border=0>
+<tr>
+<td class="pasek1-pion" width="10" height="400"></td>
+<td valign="top" class="pasek1-pion" width="200" height="400"><br><br>
+<font class="white"><b>Magnifique Agency</b></font><br><br>
+<font class="index"><b>322 N. Forest Ave<br>
+Hillside, IL 60162</b><br><br>
+<u>Phone:</u> <br><br>1 (773) 804 1266<br><br>
+<u>E-mail:</u> <br><br>amagnifiqueagency@gmail.com
+</font>
+
+
+</td>
+<td class="pasek1-pion" width="10" height="400"></td>
+<td class="pasek2-pion" width="20" height="400"></td>
+<td valign="top" class="pasek2-pion" width="540" height="400"><br><br>
+<img src="images/agency_06.jpg"><br><br><font class="white">
+Magnifique Agency is a nanny service company for children and the eldely. <br>We also offer Housekeeping and Cleaning services which could be full time, part time, live in or live out. <br>Our company is licensed, bonded and insured.<br><br>
+In a recent survey, over 97% of people over the age of 50 said that they would prefer to always remain at home, rather than move into an institutional setting, even though there has been quite a transition in the look and feel of assisted living centers.
+<br><br>
+
+<b>Please contact us for your special needs!</b>
+</font>
+
+</td>
+<td class="pasek2-pion" width="20" height="400"></td>
+
+</tr> 
+</table>
+
+<TABLE align="center" cellSpacing=0 cellPadding=0 width="800" height="40" border=0>
+
+<tr>
+<td class="t3" width="800" height="40"><p align="right">
+<b><font class="menux"><a class="menu4" href="index.php">Home</a> | <a class="menu4" href="service.php">Services</a> | <a class="menu4" href="plans.php">Plans</a> | <a class="menu4" href="area.php">Coverage Area</a> | <a class="menu4" href="contact.php">Contact Us</a></font></b>&nbsp;&nbsp;&nbsp;
+</p>
+</td>
+</tr> 
+</table>
+
+<TABLE align="center" cellSpacing=0 cellPadding=0 width="800" height="20" border=0>
+<tr>
+<td class="t4" width="780" height="20"><p align="right"><font class="footer">All Rights Reserved &copy; Magnifique Agency 2009&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font></p></td>
+</tr> 
+</table>
+
+
+</BODY>
+</HTML>
